b77bdd0000311bf2c4e3d86441e620a49332c2e0
B-2U <alexkao1439@gmail.com>
2023-08-22 19:23:36 +0800

    clean up test()

--- src/cmds/owner.rs
+++ src/cmds/owner.rs
@@ -1,107 +1,13 @@
 use std::collections::HashMap;
 
+use crate::dc_utils::Args;
 use crate::utils::structs::Linked;
 use crate::utils::LoadSaveFromJson;
 use crate::{dc_utils::ContextAddon, Context, Error};
 use poise::serenity_prelude::{ArgumentConvert, CacheHttp, Channel, ReactionType};
-use serde_json::json;
-use tokio::fs;
-
-// #[derive(Debug, Clone)]
-// struct TestView {
-//     btn_1: CreateButton,
-//     btn_2: CreateButton,
-//     btn_3: CreateButton,
-// }
-// impl Default for TestView {
-//     fn default() -> Self {
-//         TestView {
-//             btn_1: CreateButton::default()
-//                 .label("1")
-//                 .custom_id("owner_test_1")
-//                 .to_owned(),
-//             btn_2: CreateButton::default()
-//                 .label("2")
-//                 .custom_id("owner_test_2")
-//                 .to_owned(),
-//             btn_3: CreateButton::default()
-//                 .label("3")
-//                 .custom_id("owner_test_3")
-//                 .to_owned(),
-//         }
-//     }
-// }
-// impl TestView {
-//     fn build(&self) -> CreateActionRow {
-//         CreateActionRow::default()
-//             .add_button(self.btn_1.clone())
-//             .add_button(self.btn_2.clone())
-//             .add_button(self.btn_3.clone())
-//             .to_owned()
-//     }
-// }
 
 #[poise::command(prefix_command, owners_only, hide_in_help)]
-pub async fn test(_ctx: Context<'_>, #[rest] args: Option<String>) -> Result<(), Error> {
-    let _args: Vec<String> = args
-        .unwrap_or_default()
-        .split_whitespace()
-        .map(|s| s.to_string())
-        .collect();
-
-    let data = json!({
-        "name": "John",
-        "age": 30,
-        "email": "john@example.com"
-    });
-    let json_bytes = serde_json::to_vec(&data)?;
-
-    // Define the file path
-    let file_path = "output.json";
-
-    // Write the JSON data to the file
-    fs::write(file_path, json_bytes).await?;
-
-    // let mut view = TestView::default();
-    // let inter_msg = ctx
-    //     .send(|b| {
-    //         b.components(|f| f.set_action_row(view.build()))
-    //             .ephemeral(true)
-    //     })
-    //     .await?;
-    // while let Some(interaction) = inter_msg
-    //     .message()
-    //     .await?
-    //     .await_component_interactions(ctx)
-    //     .build()
-    //     .next()
-    //     .await
-    // {
-    //     match interaction.data.custom_id.as_str() {
-    //         "owner_test_1" => {
-    //             view.btn_1.disabled(true);
-    //             view.btn_2.disabled(false);
-    //             view.btn_3.disabled(false);
-    //         }
-    //         "owner_test_2" => {
-    //             view.btn_1.disabled(false);
-    //             view.btn_2.disabled(true);
-    //             view.btn_3.disabled(false);
-    //         }
-    //         "owner_test_3" => {
-    //             view.btn_1.disabled(false);
-    //             view.btn_2.disabled(false);
-    //             view.btn_3.disabled(true);
-    //         }
-    //         _ => (),
-    //     }
-    //     interaction
-    //         .create_interaction_response(ctx, |b| {
-    //             b.kind(serenity::InteractionResponseType::UpdateMessage);
-    //             b.interaction_response_data(|a| a.components(|c| c.set_action_row(view.build())))
-    //         })
-    //         .await?;
-    // }
+pub async fn test(_ctx: Context<'_>, #[rest] args: Args) -> Result<(), Error> {
     Ok(())
 }
 
