9b9af5a6eda61dfbb8b8050e932f1b5948327d93
B-2U <alexkao1439@gmail.com>
2023-09-09 23:22:16 +0800

    move error format stuffs into Display trait

--- src/utils/error_handler.rs
+++ src/utils/error_handler.rs
@@ -5,7 +5,7 @@ use poise::serenity_prelude::{self as serenity, CreateComponents, Webhook};
 
 use crate::{
     dc_utils::CreateReplyAddon,
-    utils::{self, IsacError, IsacInfo},
+    utils::{self, IsacError},
     Context, Error,
 };
 
@@ -14,10 +14,6 @@ const OOPS: &str = "***Oops! Something went wrong!***
 click the **Document** button to check the commands usage
 If this error keep coming out, please join our support server to report it
 ";
-
-const PREMIUM: &str =
-    "Seems you haven't join our Patreon, or link your discord account on Patreon yet :(
-If you do like ISAC, [take a look?]( https://www.patreon.com/ISAC_bot )";
 
 pub async fn isac_err_handler(ctx: &Context<'_>, error: &IsacError) {
     match error {
@@ -30,57 +26,8 @@ pub async fn isac_err_handler(ctx: &Context<'_>, error: &IsacError) {
             isac_get_help(ctx, Some(msg.as_ref())).await;
         }
         IsacError::Info(info) => {
-            let msg = match info {
-                IsacInfo::UserNotLinked { user_name } => match user_name.as_ref() {
-                    Some(user_name) => {
-                        format!("**{user_name}** haven't linked to any wows account yet")
-                    }
-                    None => "You haven't linked your account yet.\nEnter `/link`".to_string(),
-                },
-                IsacInfo::TooShortIgn { ign } => {
-                    format!("❌ At least 3 charactars for ign searching: `{ign}`")
-                }
-                IsacInfo::APIError { msg } => format!("❌ API error: `{msg}`"),
-                IsacInfo::InvalidIgn { ign } => format!("❌ Invalid ign: `{ign}`"),
-                IsacInfo::PlayerIgnNotFound { ign, region } => {
-                    format!("Player: `{ign}` not found in `{region}`")
-                }
-                IsacInfo::PlayerHidden { ign } => {
-                    format!("Player `{ign}`'s profile is hidden.")
-                }
-                IsacInfo::PlayerNoBattle { ign } => {
-                    format!("Player `{ign}` hasn't played any battle.")
-                }
-                IsacInfo::GeneralError { msg } => msg.clone(),
-                IsacInfo::InvalidClan { clan } => format!("❌ Invalid clan name: `{clan}`"),
-                IsacInfo::ClanNotFound { clan, region } => {
-                    format!("Clan: `{clan}` not found in `{region}`")
-                }
-                IsacInfo::ShipNotFound { ship_name } => format!("Warship: `{ship_name}` not found"),
-                IsacInfo::PlayerNoBattleShip {
-                    ign,
-                    ship_name,
-                    mode,
-                } => {
-                    format!(
-                        "Player: `{ign}` hasn't played any battle in **{ship_name}** in **{}**",
-                        mode.upper()
-                    )
-                }
-                IsacInfo::AutoCompleteError => {
-                    "❌ please select an option in the results!".to_string()
-                }
-                IsacInfo::ClanNoBattle { clan, season } => format!(
-                    "**[{}]** ({}) did not participate in season **{}**",
-                    clan.tag.replace("_", r"\_"),
-                    clan.region,
-                    season
-                ),
-                IsacInfo::NeedPremium { msg } => format!("{msg}\n{PREMIUM}"),
-                IsacInfo::EmbedPermission => format!("❌ This error means ISAC don't have to permission to send embed here, please check the **Embed Links** in the permission setting, \nOr you can just re-invite ISAC in discord to let it grant the permission"),
-            };
             let _r = ctx
-                .send(|b| b.content(msg).reply(true).ephemeral(true))
+                .send(|b| b.content(info.to_string()).reply(true).ephemeral(true))
                 .await;
         }
         IsacError::Cancelled => (),
--- src/utils/isac_error.rs
+++ src/utils/isac_error.rs
@@ -1,4 +1,4 @@
-use std::fmt;
+use std::fmt::{self, Display};
 
 use crate::{
     utils::structs::{Mode, PartialClan, Region},
@@ -43,7 +43,7 @@ impl fmt::Display for IsacError {
 pub enum IsacHelp {
     LackOfArguments,
 }
-#[derive(Debug, strum::Display, thiserror::Error)]
+#[derive(Debug, thiserror::Error)]
 pub enum IsacInfo {
     UserNotLinked {
         user_name: Option<String>, // give None if its author himself
@@ -99,6 +99,61 @@ pub enum IsacInfo {
     EmbedPermission,
 }
 
+impl Display for IsacInfo {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let msg = match self {
+            IsacInfo::UserNotLinked { user_name } => match user_name.as_ref() {
+                Some(user_name) => {
+                    format!("**{user_name}** haven't linked to any wows account yet")
+                }
+                None => "You haven't linked your account yet.\nEnter `/link`".to_string(),
+            },
+            IsacInfo::TooShortIgn { ign } => {
+                format!("❌ At least 3 charactars for ign searching: `{ign}`")
+            }
+            IsacInfo::APIError { msg } => format!("❌ API error: `{msg}`"),
+            IsacInfo::InvalidIgn { ign } => format!("❌ Invalid ign: `{ign}`"),
+            IsacInfo::PlayerIgnNotFound { ign, region } => {
+                format!("Player: `{ign}` not found in `{region}`")
+            }
+            IsacInfo::PlayerHidden { ign } => {
+                format!("Player `{ign}`'s profile is hidden.")
+            }
+            IsacInfo::PlayerNoBattle { ign } => {
+                format!("Player `{ign}` hasn't played any battle.")
+            }
+            IsacInfo::GeneralError { msg } => msg.clone(),
+            IsacInfo::InvalidClan { clan } => format!("❌ Invalid clan name: `{clan}`"),
+            IsacInfo::ClanNotFound { clan, region } => {
+                format!("Clan: `{clan}` not found in `{region}`")
+            }
+            IsacInfo::ShipNotFound { ship_name } => format!("Warship: `{ship_name}` not found"),
+            IsacInfo::PlayerNoBattleShip {
+                ign,
+                ship_name,
+                mode,
+            } => {
+                format!(
+                    "Player: `{ign}` hasn't played any battle in **{ship_name}** in **{}**",
+                    mode.upper()
+                )
+            }
+            IsacInfo::AutoCompleteError => {
+                "❌ please select an option in the results!".to_string()
+            }
+            IsacInfo::ClanNoBattle { clan, season } => format!(
+                "**[{}]** ({}) did not participate in season **{}**",
+                clan.tag.replace("_", r"\_"),
+                clan.region,
+                season
+            ),
+            IsacInfo::NeedPremium { msg } => format!("{msg}\n{PREMIUM}"),
+            IsacInfo::EmbedPermission => format!("❌ This error means ISAC don't have to permission to send embed here, please check the **Embed Links** in the permission setting, \nOr you can just re-invite ISAC in discord to let it grant the permission"),
+        };
+        write!(f, "{}", msg)
+    }
+}
+
 impl From<reqwest::Error> for IsacError {
     fn from(err: reqwest::Error) -> Self {
         IsacInfo::APIError {
@@ -107,3 +162,7 @@ impl From<reqwest::Error> for IsacError {
         .into()
     }
 }
+
+const PREMIUM: &str =
+    "Seems you haven't join our Patreon, or link your discord account on Patreon yet :(
+If you do like ISAC, [take a look?]( https://www.patreon.com/ISAC_bot )";
