37285438449eef688ac458514142f50659f0d43a
B-2U <alexkao1439@gmail.com>
2023-10-20 15:31:55 +0800

    remove ContextAddon::reply (built in in poise already)

--- src/dc_utils/context.rs
+++ src/dc_utils/context.rs
@@ -5,22 +5,12 @@ use poise::{async_trait, serenity_prelude::Typing};
 
 #[async_trait]
 pub trait ContextAddon {
-    async fn reply(
-        &self,
-        input: impl Into<String> + std::marker::Send,
-    ) -> Result<poise::ReplyHandle, poise::serenity_prelude::Error>;
     async fn typing(&self) -> MyTyping;
 }
 
 /// a trait for `reply`
 #[async_trait]
 impl ContextAddon for Context<'_> {
-    async fn reply(
-        &self,
-        content: impl Into<String> + std::marker::Send,
-    ) -> Result<poise::ReplyHandle<'_>, poise::serenity_prelude::Error> {
-        self.send(|b| b.content(content).reply(true)).await
-    }
     async fn typing(&self) -> MyTyping {
         match self {
             Context::Prefix(prefix_ctx) => {
