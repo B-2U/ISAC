9663de4f93bcf6408d9509890f2b2e273e684d76
b-2u <alexkao1439@gmail.com>
2024-02-04 01:30:06 +0800

    remove renderer debug mode

--- renderer/renderer.py
+++ renderer/renderer.py
@@ -235,4 +235,7 @@ renderer = None
 html_renderer = pystache.Renderer()
 
 if __name__ == "__main__":
-    app.run(port=3000, debug=True)
+    app.run(
+        port=3000,
+        # debug=True
+    )
