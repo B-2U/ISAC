b2a6b0b1fb941f0d4f152635c1812343f2d5bc48
Alex Kao <alexkao1439@gmail.com>
2024-07-13 23:05:26 +0800

    more informative err msg

--- src/dc_utils/user.rs
+++ src/dc_utils/user.rs
@@ -33,7 +33,7 @@ impl UserAddon for User {
         s: &str,
     ) -> Result<User, IsacError> {
         let error_message = IsacInfo::GeneralError {
-            msg: "Not a valid input, please enter an @ping or Discord User ID".to_string(),
+            msg: format!("`{s}` is not a valid user, please enter a pign like <@930855839961591849> or a Discord User ID"),
         };
 
         if s.chars().any(|c| matches!(c, '<' | '@' | '>')) || s.chars().all(|c| c.is_ascii_digit())
